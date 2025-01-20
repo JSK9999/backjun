6ca1c14c824079331300b18d56cd836090c45fa0
김재선 <js@ex-em.com>
2025-01-20 09:20:20 +0900

    문제 2750

---
+++ 2750/index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "2750/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+
+const arr = input.map(Number).slice(1);
+
+arr.sort((a, b) => a - b);
+
+let answer = "";
+for (let ix = 0; ix < arr.length; ix++) {
+  if (ix === arr.length - 1) {
+    answer += arr[ix];
+  } else {
+    answer += arr[ix] + "\n";
+  }
+}
+console.log(answer);
