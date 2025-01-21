677ae4a9747b41305e767f0c0b7e4acf7be1f13d
김재선 <js@ex-em.com>
2025-01-22 08:39:59 +0900

    문제 2751 불러오는 텍스트파일 수정

--- 2751/index.js
+++ 2751/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "dev/stdin" : "2750/input.txt";
+const filePath = process.platform === "linux" ? "dev/stdin" : "2751/input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const arr = input.map(Number).slice(1);
